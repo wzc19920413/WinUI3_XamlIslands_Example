d73d09d3693d2a09920497becda643839d9df6dc
Zhichao Wang <zhicwang@microsoft.com>
2024-12-19 15:25:05 +0800

    Add project files.

---
+++ CustomeComponent/App.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "App.h"
+#include "App.g.cpp"
+#include "trace.h"
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+namespace winrt::CustomeComponent::implementation
+{
+    App::App()
+    {
+        RegisterTraceLogger();
+        Initialize();
+        AddRef();
+        m_inner.as<::IUnknown>()->Release();
+    }
+    App::~App()
+    {
+        Close();
+    }
+}
